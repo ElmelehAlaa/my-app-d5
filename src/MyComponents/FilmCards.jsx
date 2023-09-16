4efddc9bfe4258853a224d31512daf6bcbf6c0ed
ElmelehAlaa <alaa.elmeleh@outlook.it>
2023-09-16 18:17:20 +0200

    fix -card

--- src/MyComponents/FilmCards.jsx
+++ src/MyComponents/FilmCards.jsx
@@ -8,7 +8,7 @@ class FilmCards extends Component {
         <h4 className="text-white  fs-1 text-start pb-3">{this.props.title} </h4>
         {this.props.films.map((film) => (
           <Col xs="12" sm="6" md="4" lg="3" xxl="2" className="mb-4   " key={film.imdbID}>
-            <Card title={film.Title} img={film.Poster} style={{ margin: "auto", height: "300px", border: "0px" }}>
+            <Card style={{ margin: "auto", height: "320px", border: "0px" }}>
               <Card.Img variant="top" src={film.Poster} style={{ height: "100%", objectFit: "cover" }} />
             </Card>
           </Col>
